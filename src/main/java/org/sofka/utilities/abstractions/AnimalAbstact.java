54a66d20ab13575ad2b962df470f1c503f81b976
Hernan Velasquez <velasquezmhernan@gmail.com>
2022-05-27 22:07:51 -0500

    Trabajando en los cambios del ejercicio de mentoria

---
+++ src/main/java/org/sofka/utilities/abstractions/AnimalAbstact.java
@@ -0,0 +1,10 @@
+package org.sofka.utilities.abstractions;
+
+import org.jboss.logging.Logger;
+
+public abstract class AnimalAbstact {
+    
+    private static final Logger logger = Logger.getLogger(AnimalAbstact.class);
+    
+    
+}
---
+++ src/main/java/org/sofka/utilities/interfaces/AnimalInterfaces.java
@@ -0,0 +1,9 @@
+package org.sofka.utilities.interfaces;
+
+public interface AnimalInterfaces {
+    public void slepp();
+
+    public void toEat();
+
+    public void run(); 
+}
